99f2dd476084ecee95d9a16a1eccaadc75f60886
Adnesh <adnesh.tondale@icloud.com>
2023-12-07 21:50:38 -0800

    Initial Commit

---
+++ 3D_Recycle/_D_RecycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _D_RecycleApp.swift
+//  3D_Recycle
+//
+//  Created by Adnesh Tondale on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct _D_RecycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
